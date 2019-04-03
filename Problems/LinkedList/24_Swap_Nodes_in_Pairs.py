7355d8fcc61e57364cef7e3413ec311097a86244
chendingyan <dingyan.chen96@gmail.com>
2019-04-03 10:33:39 +0100

    24 dummyhead

--- Problems/LinkedList/24_Swap_Nodes_in_Pairs.py
+++ Problems/LinkedList/24_Swap_Nodes_in_Pairs.py
@@ -19,3 +19,37 @@ class Solution(object):
             head.next = self.swapPairs(head.next.next)
             nextnode.next = head
             return nextnode
+
+# 以上是一种方法
+# 还有一种是设置一个虚拟头结点dummyHead
+#
+# 设置需要交换的两个节点分别为node1、node2，同时设置node2的下一个节点next
+#
+# 在这一轮操作中
+# 将node2节点的next设置为node1节点
+#
+# 将node1节点的next设置为next节点
+#
+# 将dummyHead节点的next设置为node2
+#
+# 结束本轮操作
+# 这里需要重点注意的是 设置dummyhead的时候 进入while循环内部的 要是它的一个reference 即设置另一个变量p
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        dummyhead = ListNode(0)
+        dummyhead.next = head
+        p = dummyhead
+        while p.next and p.next.next:
+            node1 = p.next
+            node2 = node1.next
+            nextnode = node2.next
+            node2.next = node1
+            node1.next = nextnode
+            p.next = node2
+            p = node1
+
+        return dummyhead.next
