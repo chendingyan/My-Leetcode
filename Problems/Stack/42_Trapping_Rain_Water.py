c9204d43b8194e22ed7a9a531c8db69f5cbbeccc
chendingyan <dingyan.chen96@gmail.com>
2019-04-04 10:24:56 +0800

    42

--- Problems/Stack/42_Trapping_Rain_Water.py
+++ Problems/Stack/42_Trapping_Rain_Water.py
@@ -1,6 +1,24 @@
+# Two Pointer方法
+# https://www.youtube.com/watch?v=2LjNzbK2cmA
+
 class Solution(object):
     def trap(self, height):
         """
         :type height: List[int]
         :rtype: int
         """
+        left = 0
+        right = len(height)-1
+        leftMax = 0
+        rightMax = 0
+        res = 0
+        while left < right:
+            if height[left] < height[right]:
+                leftMax = max(leftMax, height[left])
+                res += (leftMax - height[left])
+                left+=1
+            else:
+                rightMax = max(rightMax, height[right])
+                res += (rightMax - height[right])
+                right-=1
+        return res
