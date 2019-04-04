4b94dbe707b043d08232c520f2e9e209ae904f02
chendingyan <dingyan.chen96@gmail.com>
2019-04-04 08:55:32 +0800

    queue implementation

---
+++ Basics/codes/Queue.py
@@ -0,0 +1,15 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def size(self):
+        return len(self.items)
+
+    def isEmpty(self):
+        return self.size() == 0
+
+    def enqueue(self,item):
+        self.items.append(item)
+
+    def dequeue(self):
+        return self.items.pop(0)
