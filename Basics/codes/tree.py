2869d394f1332d871f8885c71b3fd04f9e793ac7
chendingyan <dingyan.chen96@gmail.com>
2019-04-05 13:07:22 +0800

    tree

---
+++ Basics/codes/tree.py
@@ -0,0 +1,43 @@
+class Node():
+    def __init__(self, elem = None, lchild = None, rchild = None):
+        self.elem = elem
+        self.lchild = lchild
+        self.rchild = rchild
+
+class Tree():
+    def __init__(self):
+        self.root = Node()
+        self.myQueue = []
+
+    def add(self, elem):
+        node = Node(elem=elem)
+        if self.root.elem == None:
+            self.root = node
+            self.myQueue.append(self.root)
+        else:
+            treenode = self.myQueue[0]
+            if not treenode.lchild:
+                treenode.lchild = node
+                self.myQueue.append(treenode.lchild)
+            elif not treenode.rchild:
+                treenode.rchild = node
+                self.myQueue.append(treenode.rchild)
+                print('---', self.myQueue)
+                self.myQueue.pop(0)
+                print(self.myQueue)
+    def preorder(self, root):
+        if not root:
+            return False
+        else:
+            print(root.elem)
+            self.preorder(root.lchild)
+            self.preorder(root.rchild)
+
+if __name__ == '__main__':
+    """主函数"""
+    elems = range(10)           #生成十个数据作为树节点
+    tree = Tree()          #新建一个树对象
+    for elem in elems:
+        tree.add(elem)           #逐个添加树的节点
+
+    tree.preorder(tree.root)
