a995bdb493f709d5f8d05dab2d0560422e9a835a
chendingyan <dingyan.chen96@gmail.com>
2019-04-12 11:34:41 +0800

    use C++

---
+++ The method of programming/1.1.cpp
@@ -0,0 +1,10 @@
+//
+// Created by MikeChen on 2019-04-12.
+//
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello, world!" << endl;
+    return 0;
+}
---
+++ The method of programming/cmake-build-debug/CMakeFiles/The_method_of_programming.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/The_method_of_programming.dir/1.1.cpp.o"
+  "The_method_of_programming.pdb"
+  "The_method_of_programming"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/The_method_of_programming.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
