c7269dab97d38d567e995ba8948a27c90a8cbfb9
chendingyan <dingyan.chen96@gmail.com>
2019-04-12 11:54:41 +0800

    C++好难写啊不记得了

--- The method of programming/1.1.cpp
+++ The method of programming/1.1.cpp
@@ -1,10 +1,48 @@
 //
 // Created by MikeChen on 2019-04-12.
 //
+// abcdef -> defabc
+// 方法一：暴力破解
 #include <iostream>
 using namespace std;
+void shiftLeftOne(char* s, int n){
+    char t = s[0];
+    for(int i = 1; i < n; i++){
+        s[i-1] = s[i];
+    }
+    s[n-1] = t;
+}
+
+char* leftRotateString(char *s, int n, int m){
+    while(m--){
+        shiftLeftOne(s, n);
+    }
+    return s;
+}
+
+
+//方法二：三步反转
+void reverse(char * s, int from, int to){
+    while (from < to){
+        char temp = s[from];
+        s[from++] = s[to];
+        s[to--] = temp;
+    }
+}
+
+char * solution(char*s){
+    reverse(s, 0, 2);
+    reverse(s, 3, 5);
+    reverse(s, 0, 5);
+    cout << s;
+
+}
+
 int main()
 {
-    cout << "Hello, world!" << endl;
+    char s[] = "abcdef";
+//    char * str = leftRotateString(s, 6, 3);
+//    cout << str;
+    solution(s);
     return 0;
 }
