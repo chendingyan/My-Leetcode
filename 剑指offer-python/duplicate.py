6a48305ca89f5ad4389877bfb8a06b7acad1784c
chendingyan <dingyan.chen96@gmail.com>
2019-04-21 11:51:21 +0800

    duplicate

---
+++ 剑指offer-python/duplicate.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+# 在一个长度为n的数组里的所有数字都在0到n-1的范围内。 数组中某些数字是重复的，但不知道有几个数字是重复的。也不知道每个数字重复几次。
+# 请找出数组中任意一个重复的数字。
+# 例如，如果输入长度为7的数组{2,3,1,0,2,5,3}，那么对应的输出是第一个重复的数字2。
+class Solution:
+    # 这里要特别注意~找到任意重复的一个值并赋值到duplication[0]
+    # 函数返回True/False
+    def duplicate(self, numbers, duplication):
+        # write code here
+        number = dict()
+        for i in numbers:
+            if i in number:
+                duplication[0] = i
+                return True, duplication[0]
+            else:
+                number[i] = 1
+        return False
+
+
+numbers = [2,1,3,1,4]
+sol = Solution()
+duplication = [100]
+print(sol.duplicate(numbers, duplication))
