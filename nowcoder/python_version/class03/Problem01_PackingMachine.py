853a147b39b5871d70347901f9753b733d27a462
chendingyan <dingyan.chen96@gmail.com>
2019-07-30 02:52:49 +0100

    PackingMachine

--- nowcoder/python_version/class03/Problem01_PackingMachine.py
+++ nowcoder/python_version/class03/Problem01_PackingMachine.py
@@ -5,5 +5,34 @@
 # 例如[1,0,5]表示有3个机器，每个机器上分别有1、0、5个物品，经过这些轮后: 第一轮:1 0<-5 => 1 1 4  第二轮:1<-1<-4 => 2 1 3  第三轮:
 # 2 1 <- 3 => 2 2 2 移动了3轮，每个机器上的物品相等，所以返回3
 # 例如[2,2,3]表示有3个机器，每个机器上分别有2、2、3个物品， 这些物品不管怎么移动，都不能使三个机器上物品数量相等，返回-1
+
+# 思路 先用单点的想法考虑 将模型简化成三个变量  restLeft x restRight
+# 分类讨论
+# 每个位置的单点 都会有一个瓶颈 其中最痛的瓶颈 就是我们要的答案
 def packingmachine(machines):
-    pass
+    sum = 0
+    for i in range (0, len(machines)):
+        sum+=machines[i]
+    if sum%len(machines)!= 0:
+        return -1
+    else:
+        avg = int (sum / len(machines))
+    maxOps = 0
+    leftSum = 0
+    for i in range(0, len(machines)):
+        rightSum = sum - leftSum - machines[i]
+        leftRes = leftSum -i * avg # 正数代表 多余了 要给出去 负数代表不够 要被支援
+        rightRes = rightSum - (len(machines) - i - 1) * avg
+        print(leftRes, rightRes)
+        if leftSum <= 0 and rightSum <= 0:
+            maxOps = max(maxOps, leftRes+rightRes)
+        else:
+            maxOps = max(maxOps, max(abs(leftRes), abs(rightRes)))
+        leftSum+=machines[i]
+
+    return maxOps
+
+if __name__ == '__main__':
+    machines = [1,0,5]
+    print(packingmachine(machines))
+
