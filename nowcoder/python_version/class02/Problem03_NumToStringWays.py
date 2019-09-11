bb4f1e33fe01db010682c5cc314a2f0caa31ba37
chendingyan <dingyan.chen96@gmail.com>
2019-09-11 22:17:48 +0100

    correct

--- nowcoder/python_version/class02/Problem03_NumToStringWays.py
+++ nowcoder/python_version/class02/Problem03_NumToStringWays.py
@@ -1,19 +1,18 @@
 # 将给定的数转换为字符串，原则如下:1对应 a，2对应b，.....26对应z，
 # 例如12258 可以转换为"abbeh", "aveh", "abyh", "lbeh" and "lyh"，个数为5，
 #  编写一个函数，给出可以转换的不同字符串的个数。
-def NumToStringWays(str):
-    dp = [0]*(len(str)+1)
-    dp[len(str)-1] = 0
-    if str[len(str)-1] != '0':
-        dp[len(str)-2] = 1
-    if int(str[len(str)-2])*10 + int(str[len(str)-1]) < 27:
-        dp[len(str)-3] = 2
+def solution(n):
+    arr = list(n)
+    dp = [0] * (len(arr)+1)
+    dp[len(arr)] = 1
+    if arr[len(arr)-1] != '0':
+        dp[len(arr)-1] = 1
     else:
-        dp[len(str)-3] = 1
-
-    for i in range(len(str)-3, -1, -1):
-        num = int(str[i-1])*10+int(str[i])
-        if num<27:
+        dp[len(arr)-1] = 0
+    for i in range(len(arr)-2, -1, -1):
+        if arr[i] == '0':
+            dp[i] = 0
+        if int(arr[i]) * 10 + int(arr[i+1]) <= 26:
             dp[i] = dp[i+1] + dp[i+2]
         else:
             dp[i] = dp[i+1]
@@ -21,4 +20,4 @@ def NumToStringWays(str):
     return dp[0]
 
 if __name__ == '__main__':
-    print(NumToStringWays('111143311'))
+    print(solution('111143311'))
