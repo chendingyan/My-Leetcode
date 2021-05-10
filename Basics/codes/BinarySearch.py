a8c7945f2d889f5970d989ea0518d4523d070c26
chendingyan <dingyan.chen96@gmail.com>
2021-05-10 19:58:40 +0800

    基础二分查找

--- Basics/codes/BinarySearch.py
+++ Basics/codes/BinarySearch.py
@@ -34,6 +34,23 @@ def binary_search_left(nums, target):
         return -1
 
 
+def binary_search_left2(nums, target):
+    # [left, right]
+    left = 0
+    right = len(nums) - 1
+    while left <= right:
+        mid = int(left + (right - left) / 2)
+        if nums[mid] == target:
+            right = mid - 1
+        elif nums[mid] > target:
+            right = mid - 1
+        elif nums[mid] < target:
+            left = mid + 1
+    if left >= len(nums) or nums[left] != target:
+        return -1
+    return left
+
+
 def binary_search_right(nums, target):
     if len(nums) == 0:
         return -1
@@ -51,9 +68,25 @@ def binary_search_right(nums, target):
     return left - 1
 
 
+def binary_search_right2(nums, target):
+    left = 0
+    right = len(nums) - 1
+    while left <= right:
+        mid = int(left + (right - left) / 2)
+        if nums[mid] == target:
+            left = mid + 1
+        elif nums[mid] > target:
+            right = mid - 1
+        elif nums[mid] < target:
+            left = mid + 1
+    if right < 0 or nums[right] != target:
+        return -1
+    return right
+
+
 if __name__ == '__main__':
     # nums = [1, 2, 3, 4, 5, 6, 7]
     nums = [1, 2, 2, 2, 3, 4, 5]
     # print(binary_search(nums, 2))
     # print(binary_search_left(nums, 2))
-    print(binary_search_right(nums, 2))
+    print(binary_search_right2(nums, 2))
